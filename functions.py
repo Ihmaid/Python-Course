01711ad15bc9d832289c23f07075ea3a85372a59
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-05-24 09:23:27 -0300

    Implemented a functional Add button at the GUI, day 17

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 FILEPATH = 'todos.txt'
 
 
-# Function that read the file and return a list with the todos
 def get_todos(filepath=FILEPATH):
     """ Read a text file and return a list of to-do items. """
     with open(filepath, 'r') as file_local:
@@ -9,7 +8,6 @@ def get_todos(filepath=FILEPATH):
     return todos_local
 
 
-# Function that receive as parameter a todos and insert at the file
 def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file. """
     with open(filepath, 'w') as file_local:
