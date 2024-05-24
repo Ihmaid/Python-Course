47d164346af9ed7c71ee99bd63adcb8016253335
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-05-24 15:18:02 -0300

    Continuation of bonusExample16, made coding exercise, day 17

--- CodingExercises/Day16Ex01/gui.py
+++ CodingExercises/Day16Ex01/gui.py
@@ -1,16 +1,29 @@
 import FreeSimpleGUI as Sg
+from functions import convert
 
 label1 = Sg.Text("Enter feet: ")
-text_box1 = Sg.InputText(tooltip="Enter the feet")
+text_box1 = Sg.InputText(tooltip="Enter the feet", key="feet")
 
 label2 = Sg.Text("Enter inches: ")
-text_box2 = Sg.InputText(tooltip="Enter the inches")
+text_box2 = Sg.InputText(tooltip="Enter the inches", key="inches")
 
-convert_button = Sg.Button("Convert")
+convert_button = Sg.Button("Convert", key="convert")
+
+result = Sg.Text(key="result")
 
 window = Sg.Window("Convertor", [[label1, text_box1],
                                  [label2, text_box2],
-                                 [convert_button]])
+                                 [convert_button, result]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    match event:
+        case "convert":
+            feet = values["feet"]
+            inches = values["inches"]
+            result = f"{round(convert(feet, inches), 3)} m"
+            window["result"].update(value=result)
+        case Sg.WIN_CLOSED:
+            break
 window.close()
