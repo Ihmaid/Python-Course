2b70b5fd2f732a7481f6f53410c8afe40ab62ba3
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-05-25 15:38:21 -0300

    Treat exceptions, add functional real-time clock, and changed theme, day 18

--- gui.py
+++ gui.py
@@ -1,7 +1,11 @@
 import functions  # Import the created library of functions
 import FreeSimpleGUI as Sg  # Import the third-party library that allows the GUI manipulations
+import time
+
+Sg.theme("LightBrown11")
 
 label = Sg.Text("Type in a to-do")
+clock = Sg.Text("", key="clock")
 
 input_box = Sg.InputText(tooltip="Enter todo", key="todo")
 add_button = Sg.Button("Add")
@@ -15,18 +19,18 @@ complete_button = Sg.Button("Complete")
 exit_button = Sg.Button("Exit")
 
 window = Sg.Window("My To-do App",
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
-    event, values = window.read()   # The read method return a tuple of 2 items, one string that is related to an event
-                                    # and a dictionary, that have key and value for all elements of the window
-
-    print(event)  # These print function are to analyse the behavior of the GUI at the terminal
-    print(values)
+    event, values = window.read(timeout=1000)   # The read method return a tuple of 2 items, one string that is related
+                                                # to an event and a dictionary, that have key and value for all elements
+                                                # of window
+    window["clock"].update(value=time.strftime('%b %d, %Y %H:%M:%S'))
 
     # Match-case block that analyse the event variable
     match event:
@@ -37,21 +41,27 @@ while True:
             functions.write_todos(todos)
             window["todos"].update(values=todos)  # The listbox is updated and now your value is the todos list
         case "Edit":
-            selected_todo = values["todos"][0]  # The [0] refers to the selected item on listbox
-            new_todo = values["todo"] + "\n"  # The new to-do is what is written at the text box
+            try:
+                selected_todo = values["todos"][0]  # The [0] refers to the selected item on listbox
+                new_todo = values["todo"] + "\n"  # The new to-do is what is written at the text box
 
-            todos = functions.get_todos()
-            index = todos.index(selected_todo)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(selected_todo)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                Sg.popup("Please select an item first.", font=("Helvetica", 20))
         case "Complete":
-            todo_to_complete = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")
+            try:
+                todo_to_complete = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+            except IndexError:
+                Sg.popup("Please select an item first.", font=("Helvetica", 20))
         case "Exit":
             break
         case "todos":
