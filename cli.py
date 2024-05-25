d818b027d32ac87b39bf3efd4c49dbf23fd26c77
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-05-25 15:13:23 -0300

    Commented the 2 main codes

--- cli.py
+++ cli.py
@@ -1,69 +1,63 @@
-import functions
-import time
+import functions  # Import created library that contains the functions of the program
+import time  # Import standard library from python
 
+# Function from library 'time' that gets the real time and print it
 now = time.strftime('%b %d, %Y %H:%M:%S')
 print(f'It is {now} ')
+
 while True:
-    # Receive the user action
+    # Receive the user action without the blank spaces
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    # Add the quest to the list
+    # If block to verify if is an add command
     if user_action.startswith("add"):
-        # Receive the user quest after the  operator add
-        todo = user_action[4:] + '\n'
-
-        todos = functions.get_todos()
-
+        # Receive an input from user then append it at the list of todos------------------------------------------------
+        # Receive the user quest after the operator add
+        todo = user_action[4:] + '\n'  # The [4:] cuts all chars before the fourth char
+        todos = functions.get_todos()  # Function to read the todos.txt
         # Append the quest at the list
         todos.append(todo.capitalize())
-
-        functions.write_todos(todos)
-    # Show the list
+        functions.write_todos(todos)  # Function to write the updated list at todos.txt
+    # If block to verify if is a show command
     elif user_action.startswith("show"):
+        # List all the items from todos with an index-------------------------------------------------------------------
         todos = functions.get_todos()
-
-        # Print the list using the enumerate function
+        # Print the list using the enumerate function, that returns an item and your related index
         for index, item in enumerate(todos):
-            item = item.strip('\n')
-            print(f'{index+1}- {item}')
-    # Edit a selected quest
+            item = item.strip('\n')  # Removes the break lines from the item
+            print(f'{index + 1}- {item}')  # Print the f string and automatically break line
+    # If block to verify if is an edit command
     elif user_action.startswith("edit"):
+        # Asks for a number input, then a new to-do that will override the number related to-do-------------------------
         # Try-except block to prevent the program to terminate because an error
         try:
             number = int(user_action[5:])
             new_todo = input('Enter a new todo: ') + '\n'
-
             todos = functions.get_todos()
-
-            # Utilize the number - 1 to access the right index of a list
-            todos[number - 1] = new_todo.capitalize()
-
+            todos[number - 1] = new_todo.capitalize()  # Utilize the number - 1 to access the right index of a list
             functions.write_todos(todos)
         except ValueError:
             print('Your command is not valid!')
             continue
-    # Remove a completed quest from the list
+    # If block to verify if is a complete command
     elif user_action.startswith("complete"):
+        # Receive a number and remove the number related to-do from the list--------------------------------------------
         try:
             number = int(user_action[9:])
-
             todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
-            todos.pop(index)
-
+            todos.pop(index)  # pop method removes an item from the list
             functions.write_todos(todos)
-
             print(f"Todo '{todo_to_remove}' was removed from the list!")
         except IndexError:
             print('This number is out of the range!')
             continue
+    # If block to verify if is an exit command
     elif user_action.startswith("exit"):
+        # Break the while loop------------------------------------------------------------------------------------------
         break
     else:
         print('Invalid Answer!')
 print("Bye!")
-
-# TIP: Use the command dir() or help() to understand and discover methods
-# TIP: import builtins and use the dir() to look at the functions
--- gui.py
+++ gui.py
@@ -1,11 +1,14 @@
-import functions
-import FreeSimpleGUI as Sg
+import functions  # Import the created library of functions
+import FreeSimpleGUI as Sg  # Import the third-party library that allows the GUI manipulations
 
 label = Sg.Text("Type in a to-do")
+
 input_box = Sg.InputText(tooltip="Enter todo", key="todo")
 add_button = Sg.Button("Add")
+
 list_box = Sg.Listbox(values=functions.get_todos(), key="todos",
-                      enable_events=True, size=(45, 10))
+                      enable_events=True, size=(45, 10))    # The get_todos returns the todos.txt list that'll be
+                                                            # displayed at the GUI
 edit_button = Sg.Button("Edit")
 
 window = Sg.Window("My To-do App",
@@ -13,19 +16,23 @@ window = Sg.Window("My To-do App",
                    font=("Helvetica", 20))
 
 while True:
-    event, values = window.read()
-    print(event)
+    event, values = window.read()   # The read method return a tuple of 2 items, one string that is related to an event
+                                    # and a dictionary, that have key and value for all elements of the window
+
+    print(event)  # These print function are to analyse the behavior of the GUI at the terminal
     print(values)
+
+    # Match-case block that analyse the event variable
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["todo"] + "\n"
+            new_todo = values["todo"] + "\n"  # Values["to-do"] get the value of dictionary values which key is to-do
             todos.append(new_todo)
             functions.write_todos(todos)
-            window["todos"].update(values=todos)
+            window["todos"].update(values=todos)  # The listbox is updated and now your value is the todos list
         case "Edit":
-            selected_todo = values["todos"][0]
-            new_todo = values["todo"] + "\n"
+            selected_todo = values["todos"][0]  # The [0] refers to the selected item on listbox
+            new_todo = values["todo"] + "\n"  # The new to-do is what is written at the text box
 
             todos = functions.get_todos()
             index = todos.index(selected_todo)
